33ef016205f4fa913dc63fb9cb86594e05068b7f
antozz996 <afiman96@gmail.com>
2026-08-25 17:39:40 +0200

    Use three-item food gallery layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { draftMode } from "next/headers";
 import { VisualEditing } from "next-sanity/visual-editing";
 import "./globals.css";
+import "./food-gallery-three.css";
 import { AnalyticsConsent } from "./components/analytics-consent";
 
 export const metadata: Metadata = {
